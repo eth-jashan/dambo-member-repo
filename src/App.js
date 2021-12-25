45c1158da91ae12d61c6960ffa99062a94d9c57a
Somesh <somesh@mudrex.com>
2021-12-25 15:56:02 +0530

    libraries and basic setup done

--- src/App.js
+++ src/App.js
@@ -1,97 +1,141 @@
 import { ethers } from "ethers";
-import { useState } from "react";
 import Web3Modal from "web3modal";
-import Portis from "@portis/web3";
+import React, { useCallback, useEffect, useState } from "react";
+import { INFURA_ID, NETWORKS } from "./constants";
 import WalletConnectProvider from "@walletconnect/web3-provider";
-import './App.css';
+import "./App.css";
+// import { Safe, SafeFactory, SafeAccountConfig } from "@gnosis.pm/safe-core-sdk";
+import { Account, GnosisSafe } from "./components";
 // import Web3 from 'web3';
+import { useUserSigner } from "./hooks";
+
+const targetNetwork = NETWORKS.rinkeby;
+const mainnetInfura = navigator.onLine
+  ? new ethers.providers.StaticJsonRpcProvider(
+      "https://mainnet.infura.io/v3/" + INFURA_ID
+    )
+  : null;
+const localProviderUrl = targetNetwork.rpcUrl;
+const localProvider = new ethers.providers.StaticJsonRpcProvider(
+  localProviderUrl
+);
+const blockExplorer = targetNetwork.blockExplorer;
 
 const providerOptions = {
-  // portis: {
-  //   package: Portis,
-  //   options: {
-  //     id: // TODO: add PORTIS id here 
-  //   }
-  // },
-  // walletconnect: {
-  //   package: WalletConnectProvider,
-  //   options: {
-  //     infuraId: // TODO: add infura ID here
-  //   }
-  // }
+  walletconnect: {
+    package: WalletConnectProvider,
+    options: {
+      infuraId: "e651a77de4974d9bae11ed4a9fcf7e57",
+    },
+  },
 };
-
 const web3Modal = new Web3Modal({
   network: "mainnet", // optional
   cacheProvider: false, // optional
-  providerOptions // required
+  providerOptions, // required
 });
 
-
 function App() {
+  const mainnetProvider = mainnetInfura;
+  const [injectedProvider, setInjectedProvider] = useState();
+  const userSigner = useUserSigner(injectedProvider, localProvider);
+  const [address, setAddress] = useState();
+  console.log('injected Provider', injectedProvider);
+  console.log('userSigner', userSigner)
+  console.log('localProvider', localProvider) 
+  useEffect(() => {
+    async function getAddress() {
+      if (userSigner) {
+        const newAddress = await userSigner.getAddress();
+        console.log('...', newAddress);
+        setAddress(newAddress);
+      }
+    }
+    getAddress();
+  }, [userSigner]);
+
+  
+
+
+  const loadWeb3Modal = useCallback(async () => {
+    const provider = await web3Modal.connect();
+    console.log('loadWeb3Modal', provider);
+    setInjectedProvider(new ethers.providers.Web3Provider(provider));
+
+    provider.on("chainChanged", chainId => {
+      console.log(`chain changed to ${chainId}! updating providers`);
+      setInjectedProvider(new ethers.providers.Web3Provider(provider));
+    });
+
+    provider.on("accountsChanged", () => {
+      console.log(`account changed!`);
+      setInjectedProvider(new ethers.providers.Web3Provider(provider));
+    });
+
+    // Subscribe to session disconnection
+    provider.on("disconnect", (code, reason) => {
+      console.log(code, reason);
+      logoutOfWeb3Modal();
+    });
+  }, [setInjectedProvider]);
+
+  const logoutOfWeb3Modal = async () => {
+    await web3Modal.clearCachedProvider();
+    if(injectedProvider && injectedProvider.provider && typeof injectedProvider.provider.disconnect == "function"){
+      await injectedProvider.provider.disconnect();
+    }
+    setTimeout(() => {
+      window.location.reload();
+    }, 1);
+  };
+
+  useEffect(() => {
+    if (web3Modal.cachedProvider) {
+      loadWeb3Modal();
+    }
+  }, [loadWeb3Modal]);
+
   return (
     <div className="App">
       <div className="App-header">
-        <Web3Func />
+        <div
+          style={{
+            position: "fixed",
+            textAlign: "right",
+            right: 0,
+            top: 0,
+            padding: 10,
+          }}
+        >
+          <div
+            style={{
+              position: "fixed",
+              textAlign: "right",
+              right: 0,
+              top: 0,
+              padding: 10,
+            }}
+          >
+            <Account
+              address={address}
+              localProvider={localProvider}
+              userSigner={userSigner}
+              mainnetProvider={mainnetProvider}
+              web3Modal={web3Modal}
+              loadWeb3Modal={loadWeb3Modal}
+              logoutOfWeb3Modal={logoutOfWeb3Modal}
+              blockExplorer={blockExplorer}
+            />
+          </div>
+        </div>
+        {/* <Web3Func /> */}
       </div>
+      <GnosisSafe 
+      userSigner={userSigner}
+      userAddress={address}
+      />
     </div>
   );
 }
-
-function Web3Func() {
-  const [account, setAccount] = useState({ connected: false });
-  
-  async function connect() {
-    const web3ModalProvider = await web3Modal.connect();
-
-    const provider = new ethers.providers.Web3Provider(web3ModalProvider);
-
-    async function setAccountFromProvider() {
-      const signer = await provider.getSigner(0);
-      const address = await signer.getAddress();
-      const balance = await signer.getBalance();
-
-      setAccount({
-        connected: true,
-        provider,
-        address,
-        signer,
-        balance: ethers.utils.formatEther(balance)
-      });
-    }
-
-    setAccountFromProvider();
-
-    web3ModalProvider.on("accountsChanged", () => {
-      setAccountFromProvider();
-    });
-  }
-
-  async function signMessage() {
-    const signedMessage = await account.signer.signMessage("Please Login to our website!");
-    console.log(signedMessage);
-  }
-
-  if(!account.connected) {
-    return (
-      <div className="button" onClick={connect}>
-        Connect to Web3!
-      </div>
-    )
-  }
-  else {
-    return (
-      <div className="account">
-        { account.address } - { account.balance }
-
-        <div className="button" onClick={signMessage}>
-          Sign Message
-        </div>
-      </div>
-    )
-  }
-
-}
-
 
 export default App;
---
+++ src/components/GnosisSafe.jsx
@@ -0,0 +1,55 @@
+import React, { useState, useCallback } from "react";
+import { useLocalStorage, useSafeSdk } from "../hooks";
+import { ethers } from "ethers";
+import {Button} from "antd";
+
+export default function GnosisSafe({
+  userSigner,
+  userAddress
+}) {
+
+  const [safeAddress, setSafeAddress] = useLocalStorage("deployedSafe");
+  const [deploying, setDeploying] = useState(false);
+  const { safeSdk, safeFactory } = useSafeSdk(userSigner, safeAddress);
+  const [owners, setOwners] = useState([userAddress]);
+  const [threshhold, setThreshhold] = useState(1);
+  console.log(userAddress)
+
+  const OWNERS = [
+    "0x34aA3F359A9D614239015126635CE7732c18fDF3",
+    "0xa81a6a910FeD20374361B35C451a4a44F86CeD46"
+  ]
+
+  const deploySafe = useCallback(
+    async (owners, threshold) => {
+      if (!safeFactory) return;
+      setDeploying(true);
+      const safeAccountConfig = { owners, threshold };
+      let safe;
+      try {
+        safe = await safeFactory.deploySafe(safeAccountConfig);
+      } catch (error) {
+        console.error(error);
+        setDeploying(false);
+        return;
+      }
+      const newSafeAddress = ethers.utils.getAddress(safe.getAddress());
+      setSafeAddress(newSafeAddress);
+    },
+    [safeFactory]
+  );
+
+  console.log('safeAddress', safeAddress);
+
+  return (
+    <div style={{ padding: 32 }}>
+      <Button
+        loading={deploying}
+        onClick={() => deploySafe(OWNERS, threshhold)}
+        type={"primary"}
+      >
+        DEPLOY SAFE
+      </Button>    
+    </div>
+  );
+}
