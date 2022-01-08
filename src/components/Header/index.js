4b3cbd53a1861ac065bd901dc4c43a878fd810ce
Jashan <jashanshetty1709@gmail.com>
2022-01-08 12:58:15 +0530

    refactoring code

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,38 +2,39 @@ import React, { useState } from "react";
 import { Menu, Typography,  Row, Col,  message, Dropdown } from "antd";
 import logoTest from '../../assets/logo-test.png'
 import 'antd/dist/antd.css'
+import styles from "./style.module.css";
 
 export default function HeaderComponent() {
 
     const [current, setCurrent] = useState('home')
 
     const renderLogo = () => (
-        <div style={{display:'flex', flexDirection:'row', width:'100%'}}>
-            <img alt={logoTest}  style={{height:'16px', width:'24px', alignSelf:'center'}} src={logoTest} />
-            <Typography.Text style={{fontFamily:'monospace', marginLeft:'12px', color:'white', alignSelf:'center', fontSize:'14px'}}>PolygonDAO</Typography.Text>
+        <div className={styles.logoContainer}>
+            <img alt={logoTest}  className={styles.img} src={logoTest} />
+            <div className={styles.logoHeading}>PolygonDAO</div>
         </div>
     )
 
-    const handleClick = e => {
-        console.log('click ', e);
-        setCurrent(e.key)
-      };
+    // const handleClick = e => {
+    //     console.log('click ', e);
+    //     setCurrent(e.key)
+    //   };
 
     const renderMenu = () => (
      <div style={{display:'flex', flexDirection:'row'}}>
-         <div style={{color:'white', fontFamily:'monospace', fontSize:'14px'}}>
+         <div className={styles.menuText}>
             Home
          </div>
-         <div style={{color:'white', fontFamily:'monospace', marginLeft:'40px', fontSize:'14px'}}>
+         <div className={styles.menuTextMiddle}>
             Treasury
          </div>
-         <div style={{color:'white', fontFamily:'monospace', marginLeft:'40px', fontSize:'14px'}}>
+         <div className={styles.menuTextMiddle}>
             Contribution
          </div>
-         <div style={{color:'white', fontFamily:'monospace', marginLeft:'40px', fontSize:'14px'}} >
+         <div className={styles.menuTextMiddle} >
             Growth
          </div>
-         <div style={{color:'white', fontFamily:'monospace', marginLeft:'40px', fontSize:'14px'}}>
+         <div lassName={styles.menuTextMiddle} >
             Settings
          </div>
      </div>   
@@ -55,7 +56,7 @@ export default function HeaderComponent() {
 
     const renderWalletPicker = () => (
         <Dropdown onClick={e => e.preventDefault()} overlay={walletMenu}>
-            <div style={{alignSelf:'center',paddingRight:'8px', paddingLeft:'8px',display:'flex',justifyContent:'center', background:'#121219'}}>
+            <div className={styles.walletContainer}>
                 <Typography.Text ellipsis={true} style={{fontFamily:'monospace', color:'white',  fontSize:'16px'}}>ETH . 0x9D7B838fF3F3A5E576da461920B633c80F040</Typography.Text>
             </div>
         </Dropdown>
@@ -63,15 +64,15 @@ export default function HeaderComponent() {
     
   return (
     <Row style={{width:'100%' ,border:'1px solid #21212B', padding:'16px',background:'black', justifyContent:'space-between', alignItems:'center'}}>
-            <Col>
-            {renderLogo()}
-            </Col>
-            <Col style={{ alignSelf:'center'}}>
-            {renderMenu()}
-            </Col>
-            <Col style={{width:'20%', alignSelf:'center'}}>
-            {renderWalletPicker()}
-            </Col>
+      <Col>
+        {renderLogo()}
+      </Col>
+      <Col style={{ alignSelf:'center'}}>
+        {renderMenu()}
+      </Col>
+      <Col style={{width:'20%', alignSelf:'center'}}>
+        {renderWalletPicker()}
+      </Col>
     </Row>
   );
 }
