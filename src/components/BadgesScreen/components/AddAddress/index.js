84adf7a851da817ea66353e0fbc493f0d395dca4
Shaurya Vardhan Singh <gsshaurya@gmail.com>
2022-06-20 17:56:58 +0530

    add single address complete

--- src/components/BadgesScreen/components/AddAddress/index.js
+++ src/components/BadgesScreen/components/AddAddress/index.js
@@ -11,6 +11,19 @@ export default function AddAddress({ selectedMembershipBadge }) {
         setAddresses((addresses) => [...addresses, ""])
     }
 
+    const updateAddress = (value, index) => {
+        const copyOfAddresses = [...addresses]
+        copyOfAddresses[index] = value
+        setAddresses(copyOfAddresses)
+    }
+
+    const deleteAddress = (deletingIndex) => {
+        const copyOfAddressesAfterDelete = [...addresses].filter(
+            (_, index) => index !== deletingIndex
+        )
+        setAddresses(copyOfAddressesAfterDelete)
+    }
+
     return (
         <div className="add-address-screen-container">
             <div className="close-minting-modal">
@@ -28,24 +41,46 @@ export default function AddAddress({ selectedMembershipBadge }) {
                     <img src={selectedMembershipBadge.imgUrl} alt="" />
                 </div>
             </div>
-            <div className="mint-membership-badge-addresses-wrapper">
-                {addresses.map((address, index) => (
-                    <div className="address-row" key={index}>
-                        <input type="text" className="address-input" />
-                        <div className="address-delete">
-                            <img src={cross} alt="" />
+            {isBulkMinting ? (
+                <div>Bulk Minting</div>
+            ) : (
+                <>
+                    <div className="mint-membership-badge-addresses-wrapper">
+                        {addresses.map((address, index) => (
+                            <div className="address-row" key={index}>
+                                <input
+                                    type="text"
+                                    className="address-input"
+                                    value={address}
+                                    onChange={(e) =>
+                                        updateAddress(e.target.value, index)
+                                    }
+                                    placeholder="Enter Address"
+                                />
+                                <div
+                                    className="address-delete"
+                                    onClick={() => deleteAddress(index)}
+                                >
+                                    <img src={cross} alt="" />
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                    <div className="add-address" onClick={addAddress}>
+                        <img src={plus_black} alt="" />
+                        Add another Address
+                    </div>
+                    <div className="minting-buttons-wrapper">
+                        <button>Mint Badges • {addresses.length}</button>
+                        <div
+                            onClick={() => setIsBulkMinting(true)}
+                            className="bulk-minting-text"
+                        >
+                            Bulk Minting
                         </div>
                     </div>
-                ))}
-            </div>
-            <div className="add-address" onClick={addAddress}>
-                <img src={plus_black} alt="" />
-                Add another Address
-            </div>
-            <div className="minting-buttons-wrapper">
-                <button>Mint Badges • {addresses.length}</button>
-                <div>Bulk Minting</div>
-            </div>
+                </>
+            )}
         </div>
     )
 }
